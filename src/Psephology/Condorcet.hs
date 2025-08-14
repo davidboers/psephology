bedf656cb63f2def7bc0f41afbf2e4838a45ef5c
davidboers <kwmmineccraft@gmail.com>
2025-08-14 16:44:36 -0400

    export `pairwiseMaj`

--- src/Psephology/Condorcet.hs
+++ src/Psephology/Condorcet.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
-module Psephology.Condorcet (condorcetWinner, condorcetMatrix, numPreferOver, smithSet) where
+module Psephology.Condorcet (condorcetWinner, condorcetMatrix, numPreferOver, smithSet, pairwiseMaj) where
 
 import Psephology.Candidate
 import Psephology.ElectoralSystem
@@ -8,18 +8,18 @@ import Psephology.Voter
 
 import Data.List
 
--- Returns the Condorcet winner if there is one.
+-- | Returns the Condorcet winner if there is one.
 condorcetWinner :: (Voter a) => [Candidate] -> [a] -> Maybe Int
 condorcetWinner candidates voters =
     let winningScore = fromIntegral $ length candidates - 1
      in findIndex ((==) winningScore . copelandScore candidates voters) [0 .. length candidates - 1]
 
--- @'numPreferOver' returns voters a b@ number of @voters@ that prefer @a@ over @b@.
+-- | @'numPreferOver' returns voters a b@ number of @voters@ that prefer @a@ over @b@.
 numPreferOver :: (Voter a) => [a] -> Candidate -> Candidate -> Int
 numPreferOver voters a b =
     head $ votes [a, b] voters
 
--- @'pairwiseMaj' voters a b@ returns the number of voters that prefer @a@ minus the number of voters that prefer @b@.
+-- | @'pairwiseMaj' voters a b@ returns the number of voters that prefer @a@ minus the number of voters that prefer @b@.
 pairwiseMaj :: (Voter a) => [a] -> Candidate -> Candidate -> Int
 pairwiseMaj voters a b =
     let [pa, pb] = votes [a, b] voters
@@ -33,8 +33,9 @@ condorcetMatrix f candidates voters =
     | b <- candidates
     ]
 
--- @'copelandScore' candidates voters c@ returns the number of other @candidates@ that @c@ beats in pairwise competitions. It's a 'Double' because
--- the [Copeland score](https://en.wikipedia.org/wiki/Copeland%27s_method#Computation) is 0.5 for tied pairs.
+{- | @'copelandScore' candidates voters c@ returns the number of other @candidates@ that @c@ beats in pairwise competitions. It's a 'Double' because
+the [Copeland score](https://en.wikipedia.org/wiki/Copeland%27s_method#Computation) is 0.5 for tied pairs.
+-}
 copelandScore :: (Voter a) => [Candidate] -> [a] -> Int -> Double
 copelandScore candidates voters c =
     sum [copelandij $ pairwiseMaj voters (candidates !! c) (candidates !! b) | b <- [0 .. length candidates - 1], c /= b]
@@ -46,14 +47,14 @@ copelandij m =
         EQ -> 0.5
         GT -> 1
 
--- @'copelandSet' candidates voters@ returns a non-empty subset of @candidates@ that have the maximum Copeland score.
+-- | @'copelandSet' candidates voters@ returns a non-empty subset of @candidates@ that have the maximum Copeland score.
 copelandSet :: (Voter a) => [Candidate] -> [a] -> [Int]
 copelandSet candidates voters =
     let copelandScores = map (copelandScore candidates voters) [0 .. length candidates - 1]
         maxCopelandScore = maximum copelandScores
      in filter (\i -> maxCopelandScore == copelandScores !! i) [0 .. length candidates - 1]
 
--- @'smithSet' candidates voters@ returns the smallest non-empty subset of @candidates@ that beat every candidate outside the set.
+-- | @'smithSet' candidates voters@ returns the smallest non-empty subset of @candidates@ that beat every candidate outside the set.
 smithSet :: (Voter a) => [Candidate] -> [a] -> [Int]
 smithSet candidates voters =
     foldl (smithSetStep candidates voters) (copelandSet candidates voters) [0 .. length candidates - 1]
