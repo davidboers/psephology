1d2701761f9d09a81eb8ed0af32c5f434b673b72
davidboers <kwmmineccraft@gmail.com>
2025-08-17 15:17:55 -0400

    Docs

--- src/Psephology/Spoilers.hs
+++ src/Psephology/Spoilers.hs
@@ -14,13 +14,15 @@ spoilers :: (Voter a) => [Candidate] -> [a] -> ElectoralSystem a -> [Int]
 spoilers candidates voters es =
     filter (isSpoiler candidates voters es) [0 .. length candidates - 1]
 
--- Does a spoilee exist when spoiler @i@ doesn't participate? @i@ cannot be a spoiler if they win with all candidates competing.
+-- | Does a spoilee exist when spoiler @i@ doesn't participate? @i@ cannot be a spoiler if they win with all candidates competing.
 isSpoiler :: (Voter a) => [Candidate] -> [a] -> ElectoralSystem a -> Int -> Bool
 isSpoiler candidates voters es i
     | es candidates voters == i = False
     | otherwise = isJust $ spoilee candidates voters es i
 
--- Returns the candidate for whom the election is spoiled if @i@ doesn't participate, or Nothing if @i@ isn't a spoiler.
+{- | @'spoilee' candidates voters es i@ returns the candidate for whom the election (under @es@) is spoiled if @i@ doesn't participate,
+or Nothing if @i@ isn't a spoiler.
+-}
 spoilee :: (Voter a) => [Candidate] -> [a] -> ElectoralSystem a -> Int -> Maybe Int
 spoilee candidates voters es i =
     let winner_with = es candidates voters
