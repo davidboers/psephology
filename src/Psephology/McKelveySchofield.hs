cffe93f628f9f43e1cadb89fc8cbd9f705a78a64
davidboers <kwmmineccraft@gmail.com>
2025-08-20 10:41:36 -0400

    Catches unmet preconditions, solving Maybe

--- src/Psephology/McKelveySchofield.hs
+++ src/Psephology/McKelveySchofield.hs
@@ -1,4 +1,3 @@
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 -- | This sub-module is dedicated to the [McKelvey-Schofield chaos theorem](https://en.wikipedia.org/wiki/McKelvey%E2%80%93Schofield_chaos_theorem).
 module Psephology.McKelveySchofield (findASpoiler, newMajority, spoilerPotential) where
 
@@ -8,26 +7,28 @@ import Psephology.Utils (integrate)
 import Psephology.Voter (preference)
 
 import Data.List
-import Data.Maybe (fromJust)
+import Data.Maybe (fromMaybe)
 
 {- | @'findASpoiler' p1 voters@ returns a new policy that is preferred by a majority of @voters@ to the current policy @p1@.
 This solution does not produce the optimal spoiler, but only the first one found.
+
+The McKelvey-Schofield theorem assumes at least 3 voters are present across at least 2 dimensions. If these conditions are
+not met, function returns the status-quo candidate (p1).
 -}
 findASpoiler :: Candidate -> [[Double]] -> Candidate
-findASpoiler p1 voters =
-    let
-        dimensions = minimum $ map length voters
-
-        makeBounds d =
-            let values = map (!! d) voters
-             in [minimum values, minimum values + 0.5 .. maximum values]
-
-        candidates = map Spacial $ transpose $ map makeBounds [0 .. dimensions - 1]
-
-        preferred p2 =
-            pairwiseMaj voters p2 p1 > 0
-     in
-        fromJust $ find preferred candidates
+findASpoiler p1 voters
+    | length voters < 3 = p1
+    | not (all ((>= 2) . length) voters) = p1
+    | otherwise =
+        let
+            dimensions = minimum $ map length voters
+            makeBounds d =
+                let values = map (!! d) voters
+                 in [minimum values, minimum values + 0.5 .. maximum values]
+            candidates = map Spacial $ transpose $ map makeBounds [0 .. dimensions - 1]
+            preferred p2 = pairwiseMaj voters p2 p1 > 0
+         in
+            fromMaybe p1 (find preferred candidates) -- fallback: should not occur if preconditions are met
 
 -- | @'newMajority' p1 voters p2@ returns a list of the indexes of @voters@ that prefer @p2@ over @p1@.
 newMajority :: Candidate -> [[Double]] -> Candidate -> [Int]
@@ -58,6 +59,7 @@ spoilerPotential p1 voters =
         iter = 1000
 
         integrate' :: [[Double]] -> Double
+        integrate' [] = 0
         integrate' (d : ds) =
             integrate f (minimum d) (maximum d) iter
           where
@@ -68,7 +70,7 @@ spoilerPotential p1 voters =
             | pairwiseMaj voters p2 p1 > 0 = 1
             | otherwise = 0
           where
-            p2 = Spacial $ l : ls
+            p2 = Spacial $ reverse (l : ls)
         integrate'' (d : ds) ls l =
             integrate f (minimum d) (maximum d) iter
           where
