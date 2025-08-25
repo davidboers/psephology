f22ee452fbb02c9b90eee518ee5084f6d274804c
davidboers <kwmmineccraft@gmail.com>
2025-08-25 16:37:30 -0400

    Weak abstentions

--- src/Psephology/Strategy/Abstention.hs
+++ src/Psephology/Strategy/Abstention.hs
@@ -1,5 +1,5 @@
 -- | Abstention as a strategy. [Read more](https://en.wikipedia.org/wiki/Participation_criterion).
-module Psephology.Strategy.Abstention where
+module Psephology.Strategy.Abstention (abstain, abstainWeak) where
 
 import Data.List (partition)
 
@@ -16,10 +16,23 @@ import Psephology.Voter
 -- Will return @Nothing@ if the strategy is impossible. First-past-the-post, the Borda count, and most
 -- rated systems are immune from the strategy (assuming no turnout requirement).
 abstain :: Voter a => [Candidate] -> [a] -> ElectoralSystem a -> Int -> Maybe (Strategy a)
-abstain candidates voters es x = do
+abstain candidates voters es x =
+    let verifyXWins voters' = es candidates voters' == x
+     in abstainWorker candidates voters verifyXWins x
+
+-- | Similar to 'abstain', except it doesn't require that @x@ win the election, only that a different
+-- candidate wins.
+abstainWeak :: Voter a => [Candidate] -> [a] -> ElectoralSystem a -> Int -> Maybe (Strategy a)
+abstainWeak candidates voters es x =
+    let actualWinner = es candidates voters
+        verifyChange voters' = es candidates voters' /= actualWinner
+     in abstainWorker candidates voters verifyChange x
+
+abstainWorker :: Voter a => [Candidate] -> [a] -> ([a] -> Bool) -> Int -> Maybe (Strategy a)
+abstainWorker candidates voters test x = do
     let (xvoters, othervoters) = partition (\vi -> preference candidates vi == x) voters
     let findAbstainers xvoters'
-            | es candidates (xvoters' ++ othervoters) == x = Just []
+            | test (xvoters' ++ othervoters) = Just []
             | null xvoters' = Nothing
             | otherwise = do
                 let abstainer = head xvoters'
