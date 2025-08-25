725dad7766010d9d64665c0e9ed07375a2c0879b
davidboers <kwmmineccraft@gmail.com>
2025-08-25 16:19:42 -0400

    Flexible voter types

--- src/Psephology/Strategy.hs
+++ src/Psephology/Strategy.hs
@@ -1,16 +1,15 @@
 -- | [Wikipedia](https://en.wikipedia.org/wiki/Strategic_voting)
 module Psephology.Strategy (Strategy (..)) where
 
-import Psephology.Candidate (Candidate)
-
 -- | A @Strategy@ is a plan amongst a group of voters to vote insincerely in a way to manipulate
 -- the election results. [Gibbard's theorem](https://en.wikipedia.org/wiki/Gibbard%27s_theorem) says
 -- that no voting system is immune from manipulation. This does not mean a particular method can be
 -- followed in every case, or that all voting systems are equally as susceptible to strategy.
-data Strategy
+data Strategy a
     = Strategy
-    { participants :: [[Candidate]]
+    { participants :: [a]
     -- ^ List of voters that must participate in a strategy.
     , newWinner :: Int
     -- ^ Winner of the election after the strategy is implemented. Represented by the index of the winning candidate in the input list.
     }
+    deriving (Eq, Show)
