b6c43730c1bdbbcf7d0b5be8290a36b0c4f6c1e6
davidboers <kwmmineccraft@gmail.com>
2025-08-25 16:20:26 -0400

    Flexible voters, y instead of x

--- src/Psephology/Strategy/Burying.hs
+++ src/Psephology/Strategy/Burying.hs
@@ -8,8 +8,8 @@ import Psephology.Voter
 
 import Data.List (delete, find, subsequences)
 
--- | @'bury' candidates voters es x@ returns a strategy that ensures @x@ loses an election under @es@.
--- The strategy will involve each participant ranking @x@ at the bottom of their ballot.
+-- | @'bury' candidates voters es y@ returns a strategy that ensures @y@ loses an election under @es@.
+-- The strategy will involve each participant ranking @y@ at the bottom of their ballot.
 --
 -- The function is as time efficient as possible, but becomes increasingly complex as the number of voters
 -- increases. The more participants needed to make the strategy work, the longer it will take to devise. If
@@ -17,10 +17,11 @@ import Data.List (delete, find, subsequences)
 --
 -- Will return @Nothing@ if the strategy is impossible. First-past-the-post, the two-round system, and instant
 -- runoff voting are immune from burying. [Read more](https://en.wikipedia.org/wiki/Later-no-harm_criterion).
-bury :: [Candidate] -> [[Candidate]] -> ElectoralSystem [Candidate] -> Int -> Maybe Strategy
-bury candidates voters es x = do
-    let votersForOtherCandidates = filter (\vi -> preference candidates vi /= x) voters
-    let bury_v = delete (candidates !! x)
-    participantsi <- find (\vs -> x /= es candidates (map bury_v vs)) (subsequences votersForOtherCandidates)
+bury :: [Candidate] -> [[Candidate]] -> ElectoralSystem [Candidate] -> Int -> Maybe (Strategy [Candidate])
+bury candidates voters es y = do
+    let votersForOtherCandidates = filter (\vi -> preference candidates vi /= y) voters
+    let bury_v = delete (candidates !! y)
+    -- This is stupid I don't know why I thought this would work
+    participantsi <- find (\vs -> y /= es candidates (map bury_v vs)) (subsequences votersForOtherCandidates)
     let newWinneri = es candidates participantsi
     return $ Strategy participantsi newWinneri
