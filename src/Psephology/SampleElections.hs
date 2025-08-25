ef799329c030fb2b7ac52ee0ab7a7ce3ba38a505
davidboers <kwmmineccraft@gmail.com>
2025-08-24 21:20:05 -0400

    docs

--- src/Psephology/SampleElections.hs
+++ src/Psephology/SampleElections.hs
@@ -13,19 +13,18 @@ import Psephology.Candidate
 
 -- Tennessee capital
 
-{- | The Tennessee example is borrowed from [Wikipedia](https://en.wikipedia.org/wiki/Template:Tenn_voting_example),
-    where it is used in several articles to explain voting systems. It contains 100 voters and 4 candidate cities
-    (Memphis, Nashville, Chattanooga, and Knoxville). The example is ideal because it has a Condorcet winner, follows
-    single-peaked preferences, and different voting systems provide different results.
-
-    @
-        let candidates = 'tennesseeCapitalCandidates'
-        let voters = 'tennesseeCapital'
-        'firstPastThePost' candidates voters    = 0      -- Memphis
-        'instantRunoffVoting' candidates voters = 3      -- Knoxville
-        'condorcetWinner' candidates voters     = Just 1 -- Nashville
-    @
--}
+-- | The Tennessee example is borrowed from [Wikipedia](https://en.wikipedia.org/wiki/Template:Tenn_voting_example),
+--     where it is used in several articles to explain voting systems. It contains 100 voters and 4 candidate cities
+--     (Memphis, Nashville, Chattanooga, and Knoxville). The example is ideal because it has a Condorcet winner, follows
+--     single-peaked preferences, and different voting systems provide different results.
+--
+--     @
+--         let candidates = 'tennesseeCapitalCandidates'
+--         let voters = 'tennesseeCapital'
+--         'firstPastThePost' candidates voters    = 0      -- Memphis
+--         'instantRunoffVoting' candidates voters = 3      -- Knoxville
+--         'condorcetWinner' candidates voters     = Just 1 -- Nashville
+--     @
 tennesseeCapital :: [[Candidate]]
 tennesseeCapital =
     replicate
@@ -41,25 +40,25 @@ tennesseeCapital =
             17
             (map Categorical ["Knoxville", "Chattanooga", "Nashville", "Memphis"])
 
-{- | Categorical Candidate instances of the four cities. -}
+-- | Categorical Candidate instances of the four cities.
 tennesseeCapitalCandidates :: [Candidate]
 tennesseeCapitalCandidates = map Categorical ["Memphis", "Nashville", "Chattanooga", "Knoxville"]
 
 -- Condorcet cycle
 
-{- | The Condorcet cycle example involves 3 candidates (A, B, C) and 20 voters. It is a minimal example of a Condorcet cycle.
-
-    5 voters: \(A \succ B \succ C\)
-    
-    8 voters: \(B \succ C \succ A\)
-
-    7 voters: \(C \succ A \succ B\)
--}
+-- | The Condorcet cycle example involves 3 candidates (A, B, C) and 20 voters. It is a minimal example of a Condorcet cycle.
+--
+--     5 voters: \(A \succ B \succ C\)
+--
+--     8 voters: \(B \succ C \succ A\)
+--
+--     7 voters: \(C \succ A \succ B\)
 condorcetCycle :: [[Candidate]]
 condorcetCycle =
     replicate 5 (map Categorical ["A", "B", "C"])
         ++ replicate 8 (map Categorical ["B", "C", "A"])
         ++ replicate 7 (map Categorical ["C", "A", "B"])
 
+-- | Categorical Candidate instances of the three candidates.
 condorcetCycleCandidates :: [Candidate]
 condorcetCycleCandidates = map Categorical ["A", "B", "C"]
