97e0104500347f9233321a7874f62a18ced998c5
davidboers <kwmmineccraft@gmail.com>
2025-09-01 00:06:47 -0400

    Section headers

--- src/Psephology/ElectoralSystems/Rated.hs
+++ src/Psephology/ElectoralSystems/Rated.hs
@@ -1,6 +1,21 @@
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
-module Psephology.ElectoralSystems.Rated where
+module Psephology.ElectoralSystems.Rated
+    ( -- * Approval voting
+      approvalVoting
+    , approvalVotes
+
+      -- * Highest median
+    , highestMedian
+
+      -- * Score voting
+    , scoreVoting
+
+      -- * STAR voting
+    , starVoting
+    , starFinalists
+    )
+where
 
 import Data.List (sortOn)
 import Data.List.Extras (argmax)
@@ -9,48 +24,45 @@ import qualified Data.Ord as Ord
 import Psephology.Candidate
 import Psephology.Condorcet (numPreferOver)
 import Psephology.Counting (scores)
-import Psephology.Voter
 import Psephology.Utils (medianI)
+import Psephology.Voter
 
 -- Approval voting
 
 -- | Returns the index of the [approval voting](https://en.wikipedia.org/wiki/Approval_voting) winner.
-approvalVoting :: (Voter a) => [Candidate] -> [a] -> Int
+approvalVoting :: Voter a => [Candidate] -> [a] -> Int
 approvalVoting candidates voters =
     let tally = approvalVotes candidates voters
      in argmax (tally !!) [0 .. length candidates - 1]
 
 -- | Reduces the voter's scoring system to a 0/1 binary, assuming they vote for all candidates with an above average score.
-approvalVotes :: (Voter a) => [Candidate] -> [a] -> [Int]
+approvalVotes :: Voter a => [Candidate] -> [a] -> [Int]
 approvalVotes =
     scores 0 1
 
 -- Highest median
 
-{- | Returns the index of the [highest median voting](https://en.wikipedia.org/wiki/Highest_median_voting_rules) winner.
-Test suite defaults to @highestMedian 0 10@
--}
-highestMedian :: (Voter a) => Int -> Int -> [Candidate] -> [a] -> Int
+-- | Returns the index of the [highest median voting](https://en.wikipedia.org/wiki/Highest_median_voting_rules) winner.
+-- Test suite defaults to @highestMedian 0 10@
+highestMedian :: Voter a => Int -> Int -> [Candidate] -> [a] -> Int
 highestMedian mn mx candidates voters =
     let medians = map (\c -> medianI $ map (\v -> score mn mx candidates v c) voters) candidates
      in argmax (medians !!) [0 .. length candidates - 1]
 
 -- Score voting
 
-{- | @'scoreVoting' mn mx candidates voters@ returns the index of the [score voting](https://en.wikipedia.org/wiki/Score_voting) winner.
-Test suite defaults to @scoreVoting 0 10@
--}
-scoreVoting :: (Voter a) => Int -> Int -> [Candidate] -> [a] -> Int
+-- | @'scoreVoting' mn mx candidates voters@ returns the index of the [score voting](https://en.wikipedia.org/wiki/Score_voting) winner.
+-- Test suite defaults to @scoreVoting 0 10@
+scoreVoting :: Voter a => Int -> Int -> [Candidate] -> [a] -> Int
 scoreVoting mn mx candidates voters =
     let tally = scores mn mx candidates voters
      in argmax (tally !!) [0 .. length candidates - 1]
 
 -- Star voting
 
-{- | @'starVoting' mn mx candidates voters@ returns the index of the [STAR voting](https://en.wikipedia.org/wiki/STAR_voting) winner.
-Test suite defaults to @starVoting 0 10@.
--}
-starVoting :: (Voter a) => Int -> Int -> [Candidate] -> [a] -> Int
+-- | @'starVoting' mn mx candidates voters@ returns the index of the [STAR voting](https://en.wikipedia.org/wiki/STAR_voting) winner.
+-- Test suite defaults to @starVoting 0 10@.
+starVoting :: Voter a => Int -> Int -> [Candidate] -> [a] -> Int
 starVoting mn mx candidates voters =
     let tally = scores mn mx candidates voters
         (f1, f2) = starFinalists tally
