d7b3d07f8934e1045b8f745d61489d6fe5d5911f
davidboers <kwmmineccraft@gmail.com>
2025-09-02 18:06:49 -0400

    Blt now supports both total and weak orderings

--- src/Psephology/BLT.hs
+++ src/Psephology/BLT.hs
@@ -1,5 +1,17 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 -- | See [here](https://yingtongli.me/git/OpenTally/about/docs/blt-fmt.md) for help regarding .blt files.
-module Psephology.BLT (fromFile, parse, export) where
+module Psephology.BLT
+    ( -- * General
+      ReadableVoter(..)
+
+      -- * Importing
+    , fromFile
+    , parse
+
+      -- * Exporting
+    , export
+    ) where
 
 import Psephology.Candidate
 import Psephology.Utils (split)
@@ -7,16 +19,45 @@ import Psephology.Utils (split)
 import Data.List
 import Data.Maybe
 
--- | @'import' pathName@ returns a tuple of ('[Candidate]', '[[[Candidate]]]'), candidates and voters.
-fromFile :: FilePath -> IO ([Candidate], [[[Candidate]]])
+-- | Only real voters can be read from a .blt file. There are two instances of this class, intended
+-- to facilitate both weak- and total-ordered preferences.
+--
+-- @
+--      'fromFile' "foo.blt" :: IO (['Candidate'], [['Candidate']])   -- For total ordering (no tied ranks)
+--      'fromFile' "bar.blt" :: IO (['Candidate'], [[['Candidate']]]) -- For weak ordering (w/tied ranks)
+-- @
+--
+-- If the blt file parsed for total ordering contains tied rankings, the candidates will be preferenced
+-- in whatever order they appear in the file. Ex. "A=B" will be read as "A, then B".
+class ReadableVoter a where
+    parsePrefs :: [Candidate] -> [String] -> a
+
+    writePrefs :: [Candidate] -> a -> [String]
+
+instance ReadableVoter [Candidate] where
+    parsePrefs candidates strs = concat (parsePrefs candidates strs :: [[Candidate]])
+
+    writePrefs candidates = map (show . ci candidates)
+
+instance ReadableVoter [[Candidate]] where
+    parsePrefs candidates = map (map (\s -> candidates !! (read s - 1)) . split '=')
+
+    writePrefs candidates = map (intercalate "=" . map (show . ci candidates))
+
+ci :: [Candidate] -> Candidate -> Int
+ci candidates c =
+    fromJust (elemIndex c candidates) + 1
+
+-- Importing
+
+-- | @'fromFile' pathName@ returns a tuple of candidates and voters.
+fromFile :: ReadableVoter a => FilePath -> IO ([Candidate], [a])
 fromFile pathName = do
     contents <- readFile pathName
     return $ parse contents
 
--- Parse
-
 -- | @'parse' text@ parses plain text to election details.
-parse :: String -> ([Candidate], [[[Candidate]]])
+parse :: ReadableVoter a => String -> ([Candidate], [a])
 parse text =
     let -- First line: number of candidates and seats (ignore seats)
         (metaLine, rest) = fromJust $ uncons $ lines text
@@ -29,34 +70,27 @@ parse text =
         parseBallotLine l =
             let ws = words l
                 num = read (head ws) :: Int
-                prefs = takeWhile (/= "0") (tail ws)
-                prefsIdx = map (map read . split '=') prefs
-             in replicate num prefsIdx
+                prefsPart = takeWhile (/= "0") (tail ws)
+             in replicate num $ parsePrefs candidates prefsPart
 
-        ballotsIdx = concatMap parseBallotLine ballotLines
+        ballots = concatMap parseBallotLine ballotLines
 
         -- Candidate names: after the "0" line, next no_candidates lines
         candidateLines = take no_candidates (drop 1 afterBallots)
         candidates = map Categorical candidateLines
-
-        -- Convert ballot indices to Candidate objects
-        ballots = map (map (map (\i -> candidates !! (i - 1)))) ballotsIdx
      in (candidates, ballots)
 
--- | Export to .blt file
-export :: [Candidate] -> [[Candidate]] -> String -> String
-export candidates voters title =
-    let prefOrdering :: [Candidate] -> String
-        prefOrdering o =
-            let num = length $ filter (o ==) voters
-             in unwords $ map show $ num : map ci o ++ [0]
+-- Exporting
 
-        ci :: Candidate -> Int
-        ci c =
-            fromJust (elemIndex c candidates) + 1
+-- | Export to .blt file
+export :: (ReadableVoter a, Eq a) => [Candidate] -> [a] -> String -> String
+export candidates voters title =
+    let ballotLine prefs =
+            let num = length $ filter (prefs ==) voters
+             in unwords $ show num : writePrefs candidates prefs ++ ["0"]
 
         header = show (length candidates) ++ " 1"
-        ballots = map prefOrdering $ nub voters
+        ballots = map ballotLine $ nub voters
         names
             | any isUnnamed candidates = map (\i -> "Candidate " ++ show i) [1 .. length candidates]
             | otherwise = map show candidates
