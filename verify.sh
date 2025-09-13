d3d52cc1434ed99f01b824a42d15e4e10cf80d66
davidboers <kwmmineccraft@gmail.com>
2025-09-13 11:52:07 -0400

    Harmonizes github workflow and verification

---
+++ verify.sh
@@ -0,0 +1,8 @@
+set +xv
+# Mirrors GitHub workflow
+cabal build all --enable-tests
+cabal run tests
+cabal run example-generate-blt
+cabal run example-georgia-redistricting
+cabal run example-sample-parliament
+bash docs.sh
