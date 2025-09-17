4a6b93c5726fa71ddc448b8c912f1d538cbd68a3
davidboers <kwmmineccraft@gmail.com>
2025-09-17 17:12:08 -0400

    MRP test

--- verify.sh
+++ verify.sh
@@ -4,5 +4,6 @@ cabal build all --enable-tests
 cabal run tests
 cabal run example-generate-blt
 cabal run example-georgia-redistricting
+cabal run example-mrp
 cabal run example-sample-parliament
 bash docs.sh
